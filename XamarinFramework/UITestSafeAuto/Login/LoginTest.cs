cdfd4401e5f3c7be824dd4147cc94b8c8f942f51
Laxminrushingha Mahapatra <34546@Hexaware.com>
2020-06-08 20:24:01 +0530

    added xamarin solution

---
+++ XamarinFramework/UITestSafeAuto/Login/LoginTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace UITestSafeAuto.Login
+{
+    public class LoginTest : BaseTest
+    {
+        public LoginTest(Platform platform) : base(platform)
+        {
+
+        }
+
+        public override void BeforeEachTest()
+        {
+            base.BeforeEachTest();
+
+            // Extra
+        }
+    }
+}
